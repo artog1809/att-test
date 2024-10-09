272ecf393c19dac75ffaa17a76c29e61587e27e4
artog1809 <gdoq17@mail.ru>
2024-10-09 23:13:15 +0300

    try to fix imgs troubles #2

--- src/cmp/header/search/index.jsx
+++ src/cmp/header/search/index.jsx
@@ -5,7 +5,7 @@ export const Search = () => {
     return (
         <form className = "form-search">
             <button type="submit">
-                <img src="./public/icn/search.svg"/>
+                <img src="/icn/search.svg"/>
             </button>
             <input type="text" placeholder="Search for something" />
         </form>
--- src/cmp/sidemenu/header/index.jsx
+++ src/cmp/sidemenu/header/index.jsx
@@ -4,7 +4,7 @@ export const Header = () => {
     return (
         <div className = "side-menu-header">
             <div className="side-menu-header_logo">
-                <img src="./public/icn/credit-card.svg" />
+                <img src="/icn/credit-card.svg" />
             </div>
             <div className="side-menu-header_text">
                 BankDash.
