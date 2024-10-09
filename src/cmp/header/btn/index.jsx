938fd3f2fd3d8ce01665dcdbd17994b356f62c2a
artog1809 <gdoq17@mail.ru>
2024-10-09 23:16:38 +0300

    try to fix imgs trpubles gh-pages #3 ;)

--- src/cmp/header/btn/index.jsx
+++ src/cmp/header/btn/index.jsx
@@ -4,7 +4,7 @@ export const Btn = ({img}) => {
 
     return (
         <button className="header-btn">
-            <img src={`/icn/${img}.svg`}/>
+            <img src={`./icn/${img}.svg`}/>
         </button>
     )
 }
--- src/cmp/sidemenu/nav/nav-elem/index.jsx
+++ src/cmp/sidemenu/nav/nav-elem/index.jsx
@@ -3,7 +3,7 @@
 export const NavElem = ({img, text}) => {
     return (
         <div className = "side-menu-nav_nav-elem">
-            <span><img src = {`/icn/${img}.svg`} /></span>
+            <span><img src = {`./icn/${img}.svg`} /></span>
             <span>{text}</span>
         </div>
     )
