9711894d8b6b5f572c9bdcabd64fcfc2be1c81cc
artog1809 <gdoq17@mail.ru>
2024-10-09 22:42:12 +0300

    add adaptive

--- src/cmp/header/index.jsx
+++ src/cmp/header/index.jsx
@@ -1,30 +1,42 @@
 import { Btn } from './btn'
 import { Search } from './search'
 import './style.css'
-
+import { useResize } from '../../use/useResize'
 
 export const Header = () => {
+
+    const {width} = useResize()
 
     const btns = [
         {id: 1, img: 'settings2'},
         {id: 2, img: 'notif'},
     ]
 
+    const content = 
+    width < 968 
+    ?
+    <>
+        <img className='header-burger' src='public/icn/bruger.svg'/>
+        <span className='header-left_text'>Settings</span>
+        <img className='header-right_user-avatar' src={`public/img/avatar.png`}/>
+    </>
+    :
+    <>
+    <div className="header-left">
+        <span className='header-left_text'>Settings</span>
+    </div>
+    <div className="header-right">
+        <Search />
+        {btns.map(el => <Btn key = {el.id} img = {el.img} />)}
+        <img className='header-right_user-avatar' src={`public/img/avatar.png`}/>
+    </div>
+    </>
+
     return (
         <div className='header'>
             <div className="header-content">
-                <div className="header-left">
-                    <span className='header-left_text'>Settings</span>
-                </div>
-                <div className="header-right">
-                    <Search />
-                    {btns.map(el => <Btn key = {el.id} img = {el.img} />)}
-                    <div className="header-right_user">
-                        <img className='header-right_user-avatar' src={`public/img/avatar.png`}/>
-                    </div>
-                </div>
+                {content}
             </div>
-            
         </div>
     )
 }
--- src/cmp/sidemenu/index.jsx
+++ src/cmp/sidemenu/index.jsx
@@ -3,8 +3,13 @@ import { Header } from './header'
 import { Nav } from './nav'
 import './style.css'
 
+import { useResize } from '../../use/useResize'
+
 export const Sidemenu = () => {
 
+    const {width} = useResize();
+
+    if(width < 968) return null;
     return (
         <div className = 'side-menu'>
             <Header />
